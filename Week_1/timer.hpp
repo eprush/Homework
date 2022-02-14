df76b99a8e952cddfad9eddcb00e413b13d80955
eprush <pavlovich.egorka@list.ru>
2022-02-14 16:28:28 +0300

    Copy vector_to_multiarray_raw.cpp in task_3

--- Week_1/timer.hpp
+++ Week_1/timer.hpp
@@ -11,7 +11,7 @@ private:
 	using microseconds_t = std::chrono::microseconds;
 
 public:
-	Timer() :m_begin(clock_t::now()), m_time(0), m_turned_on(false) {}
+	Timer() :m_begin(clock_t::now()), m_time(0), m_delta(0.0), m_turned_on(false) {}
 	~Timer() noexcept
 	{
 		try
@@ -28,18 +28,19 @@ public:
 	{
 		if (m_turned_on)
 		{
+			m_delta = std::chrono::duration_cast <microseconds_t> (clock_t::now() - m_begin).count();
 			m_turned_on = false;
-			microseconds_t delta_time(std::chrono::duration_cast <microseconds_t> (clock_t::now() - m_begin).count());
+			microseconds_t delta_time(m_delta);
 			m_time += delta_time;
-			print_time(delta_time);
+			print_time(m_delta);
 		}
 	}
 	void resume()
 	{
 		if (!m_turned_on)
 		{
-			m_turned_on = true;
 			m_begin = clock_t::now();
+			m_turned_on = true;
 		}
 	}
 
@@ -47,16 +48,21 @@ public:
 	{
 		return m_time;
 	}
+	double delta_time() const noexcept
+	{
+		return m_delta;
+	}
 
 private:
-	void print_time(microseconds_t delta_time)
+	void print_time(long long delta_time)
 	{
-		std::cout << "The working time of this section of the code is equal " << delta_time.count() << " microseconds" << std::endl;
+		std::cout << "The working time of this section of the code is equal " << delta_time << " microseconds" << std::endl;
 		std::cout << "The working time of all code is equal " << m_time.count() << " microseconds" << std::endl;
 	}
 
 private:
 	time_point_t m_begin;
 	microseconds_t m_time;
+	long long m_delta;
 	bool m_turned_on;
 };
