28b2fdaa9b568263c70c1d050b45f8de08991d75
eprush <pavlovich.egorka@list.ru>
2022-02-21 23:16:52 +0300

    Fill vector and set the same

--- Week_3/task_1.cpp
+++ Week_3/task_1.cpp
@@ -1,9 +1,21 @@
-#include <iostream>
-
+#include "timer.hpp"
+#include <random>
+#include <iterator> 
+#include <algorithm>
+#include <set>
+#include <vector>
 
 int main()
 {
+    std::mt19937 gen{ std::random_device()() };
+    std::uniform_int_distribution<int> uid(0, 100000);
+    std::vector<int> v(100000);
+    std::vector<int>::iterator begin = v.begin();
+    std::vector<int>::iterator end = v.end();
+    std::generate(begin, end, [&uid, &gen]() -> int
+        { return uid(gen); });
 
+    std::set<int> set(begin, end);
 
 	system("pause");
 	return EXIT_SUCCESS;
