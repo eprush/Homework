4aa380afbc12609a52db367b04322ac5dd12887d
eprush <pavlovich.egorka@list.ru>
2022-02-23 20:34:00 +0300

    Change task_2 in Week_2

--- Week_2/task_2.cpp
+++ Week_2/task_2.cpp
@@ -11,34 +11,31 @@
 
 int main()
 {
-    const int count = 100000;
+    const std::size_t count = 100000;
+    const std::size_t measures = 10;
     Timer t;
-    long long vector_time = 0, deque_time = 0, array_time = 0, list_time = 0, forward_list_time = 0;
-    for (auto j = 0U; j < 10; ++j)
+    long long vector_time = 0;
+    long long deque_time = 0;
+    long long array_time = 0;
+    long long list_time = 0;
+    long long forward_list_time = 0;
+    for (auto j = 0U; j < measures; ++j)
     {
         std::mt19937 gen{ std::random_device()() };
         std::uniform_int_distribution<int> uid(0, count);
-        std::vector<int> v(count);
-        std::vector<int>::iterator begin = v.begin();
-        std::vector<int>::iterator end = v.end();
-        std::generate(begin, end, [&uid, &gen]() -> int
+        std::array<int, count> array;
+        auto begin = array.begin();
+        auto end = array.end();
+        std::generate(begin, end, [&uid, &gen]()
             { return uid(gen); }); //generate 100000 random number
 
-        std::array<int, count> array;
+        std::vector<int> v(begin, end);
         std::list<int>  list(begin, end);
         std::forward_list<int> forward_list(begin, end);
         std::deque<int> deque(begin, end);
 
-        std::array<int, count>::iterator it_array = array.begin();
-
-        while (begin != end)//copy vector
-        {
-            *it_array = *begin;
-            ++begin, ++it_array;
-        }
-
         t.resume();
-        std::sort(v.begin(), v.begin() + 100000);
+        std::sort(v.begin(), v.end());
         t.pause();
         vector_time += t.delta_time();
 
@@ -48,7 +45,7 @@ int main()
         list_time += t.delta_time();
 
         t.resume();
-        std::sort(array.begin(), array.begin() + 100000);
+        std::sort(begin, end);
         t.pause();
         array_time += t.delta_time();
 
@@ -58,16 +55,16 @@ int main()
         forward_list_time += t.delta_time();
 
         t.resume();
-        std::sort(deque.begin(), deque.begin() + 100000);
+        std::sort(deque.begin(), deque.end());
         t.pause();
         deque_time += t.delta_time();
     }
    
-    std::cout << "Average vector sorting time equals " << std::setw(2) << std::right << vector_time / 10.0 << " microseconds" << std::endl <<   
-        "Average deque sorting time equals " << std::setw(2) << std::right << deque_time / 10.0 << " microseconds" << std::endl <<              
-        "Average list sorting time equals " << std::setw(4) << std::right << list_time / 10.0 << " microseconds" << std::endl <<                
-        "Average forward_list sorting time equals " << std::setw(2) << std::right << forward_list_time / 10.0 << " microseconds" << std::endl <<
-        "Average array sorting time equals " << std::setw(2) << std::right << array_time / 10.0 << " microseconds" << std::endl;                
+    std::cout << "Average vector sorting time equals " << std::setw(2) << std::right << vector_time / measures << " microseconds" << std::endl <<   
+        "Average deque sorting time equals " << std::setw(2) << std::right << deque_time / measures << " microseconds" << std::endl <<              
+        "Average list sorting time equals " << std::setw(4) << std::right << list_time / measures << " microseconds" << std::endl <<                
+        "Average forward_list sorting time equals " << std::setw(2) << std::right << forward_list_time / measures << " microseconds" << std::endl <<
+        "Average array sorting time equals " << std::setw(2) << std::right << array_time / measures << " microseconds" << std::endl;                
 
     //most often the vector is the fastest but sometimes the array is faster
 	system("pause");
