b129040332f77afc948806c26f9c5201e03652d0
eprush <pavlovich.egorka@list.ru>
2022-02-23 20:33:32 +0300

    Change task_1 in Week_3

--- Week_3/task_1.cpp
+++ Week_3/task_1.cpp
@@ -8,26 +8,29 @@
 int main()
 {
     Timer t;
-    long long vector_time = 0, set_time = 0;
-    for (auto j = 0U; j < 5; ++j)
+    long long vector_time = 0;
+    long long multiset_time = 0;
+    const std::size_t measures = 5;
+    const std::size_t count = 100000;
+    for (auto j = 0U; j < measures; ++j)
     {
         std::mt19937 gen{ std::random_device()() };
-        std::uniform_int_distribution<int> uid(0, 100000);
-        std::vector<int> v(100000);
+        std::uniform_int_distribution<int> uid(0, count);
+        std::vector<int> v(count);
         std::vector<int>::iterator begin = v.begin();
         std::vector<int>::iterator end = v.end();
-        std::generate(begin, end, [&uid, &gen]() -> int
+        std::generate(begin, end, [&uid, &gen]()
             { return uid(gen); });
 
-        std::set<int> s;
+        std::multiset<int> s;
 
         t.resume();
-        for (auto i = 0U; i < 100000; ++i)
+        for (auto i = 0U; i < count; ++i)
         {
             s.insert(v[i]);
         }
         t.pause();
-        set_time += t.delta_time();
+        multiset_time += t.delta_time();
 
         t.resume();
         std::sort(begin, end);
@@ -35,8 +38,8 @@ int main()
         vector_time += t.delta_time();
     }
 
-    std::cout << "Average vector sorting time equals " << vector_time/5.0 << " microseconds" << std::endl << //approximately 5500 microseconds
-        "Average set sorting time equals " << set_time /5.0 << " microseconds" << std::endl; //more than 25000 microseconds, in other words in 5 times more than vector
+    std::cout << "Average vector sorting time equals " << vector_time/measures << " microseconds" << std::endl << //approximately 5500 microseconds
+        "Average set sorting time equals " << multiset_time /measures << " microseconds" << std::endl; //more than 25000 microseconds, in other words in 5 times more than vector
 
 	system("pause");
 	return EXIT_SUCCESS;
