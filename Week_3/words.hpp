d7fb27b3a97ebaab139a5526d5e4690a07d99072
eprush <pavlovich.egorka@list.ru>
2022-02-25 00:59:23 +0300

    Create words.hpp

---
+++ Week_3/words.hpp
@@ -0,0 +1,7 @@
+#pragma once
+#include <chrono>
+#include <random>
+#include <set>
+#include <string>
+
+std::set < std::string > make_random_words(std::size_t N, std::size_t length); // length = 10 - good enough
