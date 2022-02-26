7c24e4a57b242578be73e19cc40a071dd02d29da
eprush <pavlovich.egorka@list.ru>
2022-02-26 22:34:30 +0300

    Change task_4

--- Week_3/task_4.cpp
+++ Week_3/task_4.cpp
@@ -17,18 +17,21 @@ struct Human
 
 using human_multi_index = multi_index_container <
 	Human, indexed_by <
-	hashed_non_unique <
-	member < Human, std::string, &Human::name > >,
-	hashed_non_unique <
-	member < Human, std::size_t, &Human::phone	> >,
-	random_access <>,
 	ordered_non_unique <
-	member < Human, std::size_t, &Human::phone > > > >;
+	member < Human, std::string, &Human::name > >,
+	random_access <>,
+	hashed_non_unique <
+	member < Human, std::size_t, &Human::phone > > ,
+	hashed_non_unique <
+	member < Human, std::string, &Human::name > >
+	> >;
 
 int main()
 {
-
-	const std::size_t vector_index = 2;
+	const std::size_t sort_index = 0;
+	const std::size_t vector_index = 1;
+	const std::size_t phone_index = 2;
+	const std::size_t name_index = 3;
 
 	human_multi_index humans;
 
@@ -36,30 +39,29 @@ int main()
 	humans.insert({ "Michael",    4242 });
 	humans.insert({ "Diana",  1245 });
 	humans.insert({ "Anna", 9958 });
+	humans.insert({ "Daria", 850 });
 
-	auto& hashed_phone_index = humans.get < 1 >();
-	std::cout << hashed_phone_index.count(4) << std::endl;
-
-	auto iterator = hashed_phone_index.find(850); 
-	hashed_phone_index.modify(iterator, [](Human& human) { human.name = "Egor"; });
-
-	std::cout << hashed_phone_index.find(0)->name << std::endl;
-	std::cout << std::endl;
-
-	const auto& ordered_phone_index = humans.get < 3 >();
-	auto begin = ordered_phone_index.lower_bound(4);
-	auto end = ordered_phone_index.upper_bound(4);
-
-	for (; begin != end; ++begin)
+	const auto& ordered_index = humans.get< sort_index >();
+	auto sort_begin = ordered_index.begin();
+	const auto sort_end = ordered_index.end();
+	for (; sort_begin != sort_end; ++sort_begin)
 	{
-		std::cout << begin->name << std::endl;
+		std::cout << sort_begin->name << std::endl;
 	}
-	std::cout << std::endl;
+	std::cout << "\n";
 
-	const auto& random_access_index = humans.get < 2 >();
-	std::cout << random_access_index[0].name << std::endl;
+	const auto& random_access_index = humans.get < vector_index >();
+	std::cout << random_access_index[4].name << std::endl << "\n";
+
+	const auto& hashed_name_index = humans.get< name_index >();
+	auto name_iterator = hashed_name_index.find("Daria");
+	std::cout << name_iterator->phone << std::endl << "\n";
+
+
+	const auto& hashed_phone_index = humans.get < phone_index >();
+	auto phone_iterator = hashed_phone_index.find(850);
+	std::cout << phone_iterator->name << std::endl << "\n";
 
 	system("pause");
-
 	return EXIT_SUCCESS;
 }
