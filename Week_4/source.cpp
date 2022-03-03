47ad10bfbd349c9c02b16e430d0fa25c3b116d87
eprush <pavlovich.egorka@list.ru>
2022-03-04 00:20:11 +0300

    Сount the number of odd numbers

--- Week_4/source.cpp
+++ Week_4/source.cpp
@@ -7,7 +7,13 @@ int main()
 
 	std::copy(std::istream_iterator<int>(std::cin), std::istream_iterator<int>(), std::back_inserter(vector));
 	std::shuffle(std::begin(vector), std::end(vector), std::default_random_engine(0));
-	std::for_each(std::begin(vector), std::end(vector), [](auto element) {std::cout << element << std::endl; });
+	std::sort(std::begin(vector), std::end(vector));
+	vector.erase(std::unique(std::begin(vector), std::end(vector)), std::end(vector));
+	int count  = std::count_if(std::begin(vector), std::end(vector), [](auto element) { if (element % 2) return element; });
+	std::cout << count << std::endl << "\n";
+
+	/*std::for_each(std::begin(vector), std::end(vector), [](auto element) {std::cout << element << std::endl; });*/
+
 
 
 	system("pause");
