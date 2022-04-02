2587ef4a660494d0a28c25bb877ff9af11dd0114
eprush <pavlovich.egorka@list.ru>
2022-04-02 17:59:37 +0300

    Modified task_1 in Week_5

--- Week_5/task_1.cpp
+++ Week_5/task_1.cpp
@@ -2,7 +2,6 @@
 #include <iomanip>
 #include <locale>
 #include <sstream>
-#include <string>
 #include <Windows.h>
 
 
@@ -11,17 +10,23 @@ int main()
 	SetConsoleCP(CP_UTF8);
 	SetConsoleOutputCP(CP_UTF8);
 
-	double money = 0.0;
-	std::cin >> money;
-
-	std::string euro;
-	std::getline(std::cin, euro);
-	std::istringstream in(euro);
+	std::stringstream in;
+	std::string str = "";
+	in << str << std::endl;
+	std::cout << "Enter a cash count in dollars ";
 	in.imbue(std::locale("en_US.UTF-8"));
-	in  >> std::get_money(euro, true);
+	long double money = 0.0;
+	std::cin >> std::get_money(money);
 
-	std::cout.imbue(std::locale("ru_RU.UTF-8"));
-	std::cout <<  std::put_money(money) << std::endl;
+
+	std::cout << "Entered count equals ";
+	std::cout.imbue(std::locale("eu_US.utf8"));
+	std::cout << std::showbase << std::put_money(money) << std::endl;
+
+	money *= 83.43;
+	std::cout << "Cash count in rubles ";
+	std::cout.imbue(std::locale("ru_RU.utf8"));
+	std::cout << std::showbase << std::put_money(money) << std::endl;
 
 	system("pause");
 	return EXIT_SUCCESS;
