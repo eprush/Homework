5b47246828f116150faec672c0b387d84c663cb9
eprush <pavlovich.egorka@list.ru>
2022-04-06 23:18:07 +0300

    Modify task_2 and graphic_of_measure in Week_7

--- Week_7/task_2.cpp
+++ Week_7/task_2.cpp
@@ -1,4 +1,4 @@
-#include "C:\Users\ASUS\OneDrive\Рабочий стол\Homework\Week_3\timer.hpp"
+#include "..\Week_3\timer.hpp"
 #include "Threads_guard.hpp"
 #include <numeric>
 #include <random>
@@ -43,29 +43,29 @@ T parallel_accumulate(Iterator first, Iterator last, std::size_t num_threads, T 
 		block_start = block_end;
 	}
 
+	init += accumulate_block < Iterator, T >()(block_start, last);
 	for (std::size_t i = 0; i < (num_threads - 1); ++i)
 	{
 		init += futures[i].get();
 	}
-	init += accumulate_block < Iterator, T >()(block_start, last);
 
 	return init;
 }
 
-void measure(std::vector<int>& v, std::size_t start = 16, std::size_t count = 500)
+void measure(const std::vector<double>& v, std::size_t start = 16, std::size_t count = 500)
 {
 	Timer t;
 	std::ofstream outf("measures.xls");
 	if (!outf)
 	{
-		throw std::ios_base::failure("File could not be opened for writing");
+		throw std::runtime_error("File could not be opened for writing");
 	}
 
-	int result = 0;
+	auto result = 0.0;
 	for (auto i = 0U; i < count; ++i)
 	{
 		t.resume();
-		result = parallel_accumulate(std::begin(v), std::end(v), start + i, 0);
+		result = parallel_accumulate(std::begin(v), std::end(v), start + i, 0.0);
 		t.pause();
 		outf << t.delta_time() << std::endl;
 	}
@@ -78,7 +78,7 @@ std::size_t optimize(std::size_t start = 16, std::size_t count = 500)
 
 	if (!inf)
 	{
-		throw std::ios::failure("File could not be opened for reading");
+		throw std::runtime_error("File could not be opened for reading");
 	}
 
 	double longer_time = 0.0, less_time = 0.0;
@@ -97,19 +97,19 @@ std::size_t optimize(std::size_t start = 16, std::size_t count = 500)
 
 int main()
 {
-	std::vector < int > v(100);
-	std::iota(std::begin(v), std::end(v), 1);
+	std::vector < double > v(100000);
+	std::iota(std::begin(v), std::end(v), 1.0);
 
 	try 
 	{ 
 		measure(v); 
-		std::cout << "The most optimal count of currents equals " << optimize() << std::endl;//19
+		std::cout << "The most optimal count of currents equals " << optimize() << std::endl;//17
 	}
-	catch (std::ios_base::failure& exception)
+	catch (const std::runtime_error& exception)
 	{
-		std::cerr << "Standard exception: " << exception.what() << " in " << __LINE__ << " line" <<  std::endl;
+		std::cerr << "Runtime exception: " << exception.what() << " in " << __LINE__ << " line" <<  std::endl;
 	}
-	catch (std::exception& exception)
+	catch (const std::exception& exception)
 	{
 		std::cerr << "Standard exception: " << exception.what() << " in " << __LINE__ << " line" << std::endl;
 	}
