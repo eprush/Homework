48cdef76e2ad1688095d2b768149b27904b59b51
eprush <pavlovich.egorka@list.ru>
2022-04-11 19:41:36 +0300

    Modify task_1 in Week_5

--- Week_5/task_1.cpp
+++ Week_5/task_1.cpp
@@ -10,12 +10,9 @@ int main()
 	SetConsoleCP(CP_UTF8);
 	SetConsoleOutputCP(CP_UTF8);
 
-	std::stringstream in;
-	std::string str = "";
-	in << str << std::endl;
 	std::cout << "Enter a cash count in dollars ";
-	in.imbue(std::locale("en_US.UTF-8"));
 	long double money = 0.0;
+	const double exchange_rate = 83.43;
 	std::cin >> std::get_money(money);
 
 
@@ -23,7 +20,7 @@ int main()
 	std::cout.imbue(std::locale("eu_US.utf8"));
 	std::cout << std::showbase << std::put_money(money) << std::endl;
 
-	money *= 83.43;
+	money *= exchange_rate;
 	std::cout << "Cash count in rubles ";
 	std::cout.imbue(std::locale("ru_RU.utf8"));
 	std::cout << std::showbase << std::put_money(money) << std::endl;
