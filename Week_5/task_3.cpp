ee2f488e8772b294c39c3c2a8b936034fc195106
eprush <pavlovich.egorka@list.ru>
2022-04-16 22:24:12 +0300

    Modify task_3 in Week_5

--- Week_5/task_3.cpp
+++ Week_5/task_3.cpp
@@ -1,23 +1,19 @@
 #include <iostream>
-#include <locale>
 #include <string>
 #include <regex>
+#include <numeric>
 
 int main()
 {
-    std::cout.imbue(std::locale("eu_US.UTF-8"));
-    std::string str("srhdfhh_...2002 pavlovich.er@phystech.edu@@mail.ru@@ 2022_ dest..royer@gmail.com 2345yjhgf ");
-    const std::regex regex(R"(\w+\.{1}\w+@(\w+\.\w+))");
-    std::smatch match;
-
-    while (std::regex_search(str, match, regex))
-    {
-        for (auto i = 0U; i < std::size(match); i += 2)
+    std::string str("sr admin@mail.mipt.ru hdfhh_...2002 pavlovich.er@phystech.edu@@mail.ru@@ 2022_ dest..royer@gmail.com 2345yjhgf i.s.m.mipt@yandex.ru ");
+    const std::regex regex( R"((\w+\.?)+\w+@((\w+\.?)+\w+))" );
+    
+    std::for_each(std::sregex_iterator(std::begin(str), std::end(str), regex), std::sregex_iterator(), [](auto i)
         {
-            std::cout << "Email adress: " << match[i] << " Domain name: " << match[i + 1] << std::endl;
-        }
-        str = match.suffix().str();
-    }
+            std::cout << i.str() << ' ' << "Domain name is " << i[2].str() << std::endl;
+        });
+
+
     //pavlovich.er@phystech.edu - yes
     //dest..royer@gmail.com - no, because two points
 
